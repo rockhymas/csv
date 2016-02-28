78ebd3ea0e255e00ae4fb7254652f91867010fd8
Rock Hymas <rhymas@articulate.com>
2016-02-28 15:41:45 -0700

    Add new printrow command

--- Csv.Test/CsvControllerSpec.cs
+++ Csv.Test/CsvControllerSpec.cs
@@ -9,11 +9,11 @@ namespace Csv.Test
     {
         public void Main()
         {
-            string fileName = null;
+            string[] args = null;
 
-            When("executing the csv controller", () => SUT.Execute(new []{ "print", fileName }));
+            When("executing the csv controller", () => SUT.Execute(args));
 
-            Given("the command to execute is 'print file.csv'", () => fileName = "file.csv").Verify(() =>
+            Given("the command to execute is 'print file.csv'", () => args = new []{ "print", "file.csv" }).Verify(() =>
             {
                 Given("there is no file called 'file.csv'", () => Get<IFileSystem>().Stub(f => f.FileExists(Arg<string>.Is.Equal("file.csv"))).Return(false)).Verify(() =>
                     Then("output 'There is no 'file.csv''", () => AssertWasCalled<IConsole>(c => c.Writeline(Arg<string>.Is.Equal("There is no 'file.csv'")))));
@@ -24,9 +24,34 @@ namespace Csv.Test
 
             });
 
-            Given("the command to execute is 'print file2.csv'", () => fileName = "file2.csv").Verify(() =>
+            Given("the command to execute is 'print file2.csv'", () => args = new[] { "print", "file2.csv" }).Verify(() =>
             Given("there is no file called 'file2.csv'", () => Get<IFileSystem>().Stub(f => f.FileExists(Arg<string>.Is.Equal("file2.csv"))).Return(false)).Verify(() =>
                 Then("output 'There is no 'file2.csv''", () => AssertWasCalled<IConsole>(c => c.Writeline(Arg<string>.Is.Equal("There is no 'file2.csv'"))))));
+
+            Given("the command to execute is 'printrow file.csv 2'", () => args = new[] { "printrow", "file.csv", "2" }).Verify(() =>
+            {
+                Given("there is no file called 'file.csv'", () => Get<IFileSystem>().Stub(f => f.FileExists(Arg<string>.Is.Equal("file.csv"))).Return(false)).Verify(() =>
+                    Then("output 'There is no 'file.csv''", () => AssertWasCalled<IConsole>(c => c.Writeline(Arg<string>.Is.Equal("There is no 'file.csv'")))));
+
+                Given("there is a file called 'file.csv'", () => Get<IFileSystem>().Stub(f => f.FileExists(Arg<string>.Is.Equal("file.csv"))).Return(true)).Verify(() =>
+                {
+                    Given("'file.csv' contains 'the contents of file.csv'", () => Get<IFileSystem>().Stub(f => f.OpenFile(Arg<string>.Is.Equal("file.csv"))).Return(new MemoryStream(ASCIIEncoding.Default.GetBytes("the contents of file.csv")))).Verify(() =>
+                        Then("output ''file.csv' does not contain row 2", () => AssertWasCalled<IConsole>(c => c.Writeline(Arg<string>.Is.Equal("'file.csv' does not contain row 2")))));
+
+                    Given("'file.csv' contains 'row 1 of file.csv\nrow 2 of file.csv'", () => Get<IFileSystem>().Stub(f => f.OpenFile(Arg<string>.Is.Equal("file.csv"))).Return(new MemoryStream(ASCIIEncoding.Default.GetBytes("row 1 of file.csv\nrow 2 of file.csv")))).Verify(() =>
+                        Then("output 'row 2 of file.csv'", () => AssertWasCalled<IConsole>(c => c.Writeline(Arg<string>.Is.Equal("row 2 of file.csv")))));
+
+                });
+            });
+
+            Given("the command to execute is 'printrow file.csv a'", () => args = new[] { "printrow", "file.csv", "a" }).Verify(() =>
+            {
+                Given("there is no file called 'file.csv'", () => Get<IFileSystem>().Stub(f => f.FileExists(Arg<string>.Is.Equal("file.csv"))).Return(false)).Verify(() =>
+                    Then("output 'There is no 'file.csv''", () => AssertWasCalled<IConsole>(c => c.Writeline(Arg<string>.Is.Equal("There is no 'file.csv'")))));
+
+                Given("there is a file called 'file.csv'", () => Get<IFileSystem>().Stub(f => f.FileExists(Arg<string>.Is.Equal("file.csv"))).Return(true)).Verify(() =>
+                    Then("output ''a' is not a valid row'", () => AssertWasCalled<IConsole>(c => c.Writeline(Arg<string>.Is.Equal("'a' is not a valid row")))));
+            });
         }
     }
 }
--- Csv/CsvController.cs
+++ Csv/CsvController.cs
@@ -15,17 +15,53 @@ namespace Csv
 
         public void Execute(string[] args)
         {
-            if (fileSystem.FileExists(args[1]))
+            if (args[0] == "print")
             {
-                using (var stream = fileSystem.OpenFile(args[1]))
-                using (var streamReader = new StreamReader(stream))
+                if (fileSystem.FileExists(args[1]))
                 {
-                    console.Writeline(streamReader.ReadToEnd());
+                    using (var stream = fileSystem.OpenFile(args[1]))
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        console.Writeline(streamReader.ReadToEnd());
+                    }
+                }
+                else
+                {
+                    console.Writeline(string.Format("There is no '{0}'", args[1]));
                 }
             }
-            else
+            else if (args[0] == "printrow")
             {
-                console.Writeline(string.Format("There is no '{0}'", args[1]));
+                if (fileSystem.FileExists(args[1]))
+                {
+                    var row = 0;
+                    if (!int.TryParse(args[2], out row))
+                    {
+                        console.Writeline(string.Format("'{0}' is not a valid row", args[2]));
+                        return;
+                    }
+
+                    using (var stream = fileSystem.OpenFile(args[1]))
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        while (row > 0 && !streamReader.EndOfStream)
+                        {
+                            var rowContents = streamReader.ReadLine();
+                            row--;
+                            if (row == 0)
+                            {
+                                console.Writeline(rowContents);
+                                return;
+                            }
+                        }
+
+                        console.Writeline(string.Format("'{0}' does not contain row {1}", args[1], args[2]));
+                    }
+                }
+                else
+                {
+                    console.Writeline(string.Format("There is no '{0}'", args[1]));
+                }
             }
         }
     }
